ca04731d48d5da15bc289049cd73b730d22c3fc0
Daichi Koyanagi <takanogi2468@gmail.com>
2024-12-14 14:21:54 +0100

    refactor:rename the folder (posts) to (blogs)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
   const posts: PostType[] = await getCachedPostsForHeadline();
 
   return (
-    <Section className="">
+    <Section className="" >
       <section className="flex justify-center items-center min-h-[calc(100vh-72px)]">
         {/* hero image start */}
         <Image
